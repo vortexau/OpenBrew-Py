8172377ea76e0a9cbda3f78c5d20e047076c1dbf
OpenBrew <openbrew@openbrew-vm>
2015-11-06 01:15:26 +1030

    moved code into a src directory

--- brewery/PumpHLT.py
+++
@@ -1,8 +0,0 @@
-
-
-class PumpHlt(Pump):
-
-    def __init__(self, logger, dbconn):
-        super(PumpHlt, self).__init__(logger, dbconn)    
-        self.set_name("HLT Recirc Pump")
-        self.set_type(self.PUMP_BROWN)
--- brewery/PumpRims.py
+++
@@ -1,8 +0,0 @@
-
-
-class PumpRims(Pump):
-
-    def __init__(self, logger, dbconn):
-        super(PumpRims, self).__init__(logger, dbconn)    
-        self.set_name("RIMS Recirc Pump")
-        self.set_type(self.PUMP_MARCH)
