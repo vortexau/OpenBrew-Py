8bda2e3524c123b0e8a790b0a89f53a33838cd5e
James McLean <james.mclean@gmail.com>
2015-11-06 15:39:17 +1030

    empty script for emulating a ds18b20 sensor

---
+++ brewery/scripts/emulate_ds18b20.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+# Script to emulate a DS18B20 sensor.
+# This will just output a string to a file like a 'real' sensor would do, 
+# with the idea being that a development VM for example can then read the 'sensor' without having a real one installed.
