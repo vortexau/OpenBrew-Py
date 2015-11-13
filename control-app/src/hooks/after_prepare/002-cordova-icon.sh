c4857d9401c52daf9406f95d023bea295dbb4c0d
James Mclean <james@geonosis.local>
2015-11-14 09:53:40 +1030

    added check to the cordova icon hook to generate a nice message

--- control-app/src/hooks/after_prepare/002-cordova-icon.sh
+++ control-app/src/hooks/after_prepare/002-cordova-icon.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-cordova-icon
+if cordova-icon ; then
+    echo "Cordova Icon succeeded"
+else
+    echo "Have you run 'npm install cordova-icon' and 'npm install cordova-icon -g'? Have you got ImageMagick installed?"
