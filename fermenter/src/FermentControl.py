9ebb3350ee71025d64c72d348729dff986fb6f56
James McLean <james.mclean@gmail.com>
2015-11-21 08:44:15 +0000

    Added beginnings of threading to the ferment control program

--- fermenter/src/FermentControl.py
+++ fermenter/src/FermentControl.py
@@ -1,2 +1,31 @@
 
+import os
+
+class FermentControl:
+
+    THREADS = 3
+
+    def __init__(self):
+        print("OpenBrew - Ferment")
+
+
+    def run(self):
+        print("Running...")
+
+        children = []
+
+        for process in range(self.THREADS):
+            pid = os.fork()
+            if pid:
+                children.append(pid)
+            else:
+                self.runFermentThread(process, pid)
+                os._exit(0)
+
+        for i, child in enumerate(children):
+            os.waitpid(child, 0)
+
+
+    def runFermentThread(self, process, pid):
+        print 'This is process thread {0}, with PID {1} for fermenter tasks'.format(process, pid)
 
