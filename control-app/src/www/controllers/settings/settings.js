dcd43699683e5023cfa79c4a0a6cd3a4d9f52864
James McLean <james.mclean@gmail.com>
2015-11-28 19:59:49 +1030

    added a controller for settings page

---
+++ control-app/src/www/controllers/settings/settings.js
@@ -0,0 +1,9 @@
+    app.controller('SettingsController', ['$scope', function SettingsController($scope) {
+        $scope.thing = 'Hello, world!';
+
+        $scope.breweryaddress = '192.168.1.23';
+        $scope.fermentoraddress = '192.168.1.62';
+        $scope.kegeratoraddress = '192.168.1.233';
+    }]);
+
+
