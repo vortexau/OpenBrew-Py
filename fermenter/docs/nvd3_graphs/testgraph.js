2471d7a14b66f08290cfe95b9a7c977001d88883
James McLean <james.mclean@gmail.com>
2015-12-01 22:26:10 +1030

    messing with a new graph system

--- fermenter/docs/nvd3_graphs/testgraph.js
+++ fermenter/docs/nvd3_graphs/testgraph.js
@@ -23,7 +23,7 @@ angular.module('app', ['nvd3'])
                     axisLabel: 'Time (ms)'
                 },
                 yAxis: {
-                    axisLabel: 'Voltage (v)',
+                    axisLabel: 'Temperature (c)',
                     tickFormat: function(d){
                         return d3.format('.02f')(d);
                     },
@@ -35,24 +35,8 @@ angular.module('app', ['nvd3'])
             },
             title: {
                 enable: true,
-                text: 'Title for Line Chart'
+                text: 'Fermenter One - Temp Chart'
             },
-            subtitle: {
-                enable: true,
-                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
-                css: {
-                    'text-align': 'center',
-                    'margin': '10px 13px 0px 7px'
-                }
-            },
-            caption: {
-                enable: true,
-                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
-                css: {
-                    'text-align': 'justify',
-                    'margin': '10px 13px 0px 7px'
-                }
-            }
         };
 
         $scope.data = sinAndCos();
@@ -73,19 +57,18 @@ angular.module('app', ['nvd3'])
             return [
                 {
                     values: sin,      //values - represents the array of {x,y} data points
-                    key: 'Sine Wave', //key  - the name of the series.
+                    key: 'Ambient', //key  - the name of the series.
                     color: '#ff7f0e'  //color - optional: choose your own line color.
                 },
                 {
                     values: cos,
-                    key: 'Cosine Wave',
+                    key: 'Air',
                     color: '#2ca02c'
                 },
                 {
                     values: sin2,
-                    key: 'Another sine wave',
+                    key: 'Wort',
                     color: '#7777ff',
-                    area: true      //area - set to true if you want this line to turn into a filled area chart.
                 }
             ];
         };
