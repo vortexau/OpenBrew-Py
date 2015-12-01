a79acb9af46dbcbc1aff210cbf1cc8ffd8c81c6e
James McLean <james.mclean@gmail.com>
2015-12-01 13:37:10 +1030

    added example generation of the correct JSON structure with python

--- control-app/src/www/controllers/settings/settings.js
+++ control-app/src/www/controllers/settings/settings.js
@@ -2,8 +2,13 @@
         $scope.thing = 'Hello, world!';
 
         $scope.breweryaddress = '192.168.1.23';
+        $scope.breweryaddressport = '1056';
+
         $scope.fermentoraddress = '192.168.1.62';
+        $scope.fermentoraddressport = '1469';
+
         $scope.kegeratoraddress = '192.168.1.233';
+        $scope.kegeratoraddressport = '1272';
     }]);
 
 
