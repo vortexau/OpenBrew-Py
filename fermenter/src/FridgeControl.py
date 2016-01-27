5981d299b3efc92504df1b42a805787a6df2704c
James McLean <james.mclean@gmail.com>
2016-01-27 16:19:15 +1030

    eh.

--- fermenter/src/FridgeControl.py
+++ fermenter/src/FridgeControl.py
@@ -15,15 +15,31 @@ class FridgeControl:
         self.fermentor = fermentor
 
         readwait = self.config.get('fermentor','readwait')
+        nofermwait = self.config.get('fermentor','nofermwait')
         
         while True:
             fermenting = False
             print 'Actions to control fermentor {0}'.format(fermentor)
 
             # Is there a fermenting batch at this moment?
+            # Check the DB to find out.
             if fermenting = False:
-                readwait = 3600 # 1 hour. 
-            else:
-                print "Sleeping for {0}".format(readwait)
+                print "Nothing fermenting. Sleeping for {0}".format(nofermwait)
+                time.sleep(float(nofermwait))
+                continue
 
+
+            # Check the wort temp
+
+            # Check the air temp
+
+            # Check the ambient?
+
+            # Heating circuit is...
+
+            # Cooling circuit is...
+
+            # Defined setpoint at this point? Check the steps in the beerbatch
+
+            print "Sleeping for {0}".format(readwait)
             time.sleep(float(readwait))
